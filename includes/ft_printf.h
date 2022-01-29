c584421a84011bc43fcdfb983dae9423ebee6e2d
tomori <tmrtkm1227tksx@gmail.com>
2022-01-29 10:33:30 +0900

    [add] Simple header file

---
+++ includes/ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/29 10:30:03 by ttomori           #+#    #+#             */
+/*   Updated: 2022/01/29 10:32:30 by ttomori          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdint.h>
+
+int	ft_printf(const char *format, ...);
+
+#endif

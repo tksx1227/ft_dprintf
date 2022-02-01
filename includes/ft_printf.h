f44b403efe03f08087925b698089a1c31bbe3a0d
tomori <tmrtkm1227tksx@gmail.com>
2022-02-02 00:44:03 +0900

    [add] Structure of t_print

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/29 10:30:03 by ttomori           #+#    #+#             */
-/*   Updated: 2022/01/30 17:50:56 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/02/01 23:25:05 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,20 @@
 # include <stdarg.h>
 # include <stdbool.h>
 # include "../libft/includes/libft.h"
+
+typedef struct s_print
+{
+	int		wc;
+	int		width;
+	char	spec;
+	char	*content;
+	bool	sharp_flag;
+	bool	space_flag;
+	bool	plus_flag;
+	bool	minus_flag;
+	bool	zero_flag;
+	bool	dot_flag;
+}	t_print;
 
 int		ft_putchar(char c);
 int		ft_putstr(char *s);

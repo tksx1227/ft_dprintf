36af25fd3dcc251feb4571c619c5890e36c67c4e
tomori <tmrtkm1227tksx@gmail.com>
2022-02-02 13:58:30 +0900

    [update] Replace add_prefix to add_prefix_with_free

--- srcs/ft_parser_str.c
+++ srcs/ft_parser_str.c
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 00:38:29 by ttomori           #+#    #+#             */
-/*   Updated: 2022/02/02 12:07:56 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/02/02 13:57:38 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,17 @@ bool	ft_parse_str(va_list *ap, t_print *info)
 bool	ft_parse_ptr(va_list *ap, t_print *info)
 {
 	char				*s;
-	char				*tmp;
+	char				*prefix;
 	unsigned long long	n;
 
 	n = (unsigned long long)va_arg(*ap, void *);
 	s = ft_itoa_base(n, 16, true);
 	if (s == NULL)
 		return (false);
-	tmp = s;
-	s = add_prefix(s, "0x");
-	free(tmp);
+	prefix = ft_strdup("0x");
+	if (prefix == NULL)
+		return (false);
+	s = add_prefix_with_free(s, prefix);
 	if (s == NULL)
 		return (false);
 	info->content = s;

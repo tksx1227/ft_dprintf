1e7c2273c363626b87bd76ec205849ee247685c1
tomori <tmrtkm1227tksx@gmail.com>
2022-02-03 01:54:40 +0900

    [add] ft_printf function

---
+++ srcs/ft_printf.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/29 10:28:29 by ttomori           #+#    #+#             */
+/*   Updated: 2022/02/03 01:54:29 by ttomori          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf.h"
+
+int	ft_printf(const char *fmt, ...)
+{
+	int		wc;
+	int		ret;
+	va_list	ap;
+	t_print	info;
+
+	ret = 0;
+	va_start(ap, fmt);
+	clear_info(&info, NULL);
+	while (*fmt != '\0' && ret != -1)
+	{
+		if (*fmt == '%')
+		{
+			fmt++;
+			if (ft_parser((char **)&fmt, &ap, &info) && ft_attacher(&info))
+				wc = ft_putval(info.content, ret);
+			else
+				wc = -1;
+			clear_info(&info, free);
+		}
+		else
+			wc = ft_put2per((char **)&fmt);
+		ret = add_write_count(ret, wc);
+	}
+	va_end(ap);
+	return (ret);
+}
